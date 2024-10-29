661248d6eb1612c27fb5a571b4f3432f13f706a9
rashmika <rashmikadil2023@gmail.com>
2024-10-29 12:46:40 -0700

    JS Class

---
+++ L02-class.js
@@ -0,0 +1,11 @@
+class car {
+    constructor(color){
+        this.color = color ;
+        this.model = "R8"
+    }
+}
+
+let blueCar = new car ("blue");
+
+console.log(blueCar.color)
+console.log(blueCar.model)
