a3c356dfc614bf9d1d4485a2f38416d669947a53
rashmika <rashmikadil2023@gmail.com>
2024-11-01 12:53:44 -0700

    this code will : calculate area in rectangle

---
+++ projects/index.js
@@ -0,0 +1,7 @@
+function calculate(){
+    let h = document.getElementById('height').value
+    let b = document.getElementById('base').value
+    let a = 0.5 * b * h
+    document.getElementById('result').innerHTML = a 
+    //Calculate area in rectangle
+}
